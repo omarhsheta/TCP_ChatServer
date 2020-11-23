825628043f1618d25db2792a0c1c44f7c395e5a6
osh18 <osh18@uclive.ac.nz>
2020-11-23 15:11:58 +1300

    Server and client finally fully works, tweaking to be done later

---
+++ Client.py
@@ -0,0 +1,41 @@
+import socket
+import threading
+def receive(client, nickname):
+    while True:
+        try:
+            message = client.recv(1024).decode("ascii")
+            if message == 'NICK':
+                client.send(nickname.encode("ascii"))
+            else:
+                print(message)
+        except:
+            print("ERROR: Something went wrong... Closing the client!")
+            client.close()
+            break
+
+def write(client, nickname):
+    while True:
+        message = f"{nickname}: {input('')}"
+        try:
+            client.send(message.encode("ascii"))
+        except OSError:
+            print("ERROR: You failed to send the message to the server! Closing the client!")
+            break
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("CLIENT: Please enter a proper port number...")
+    port = int(input())
+    try:
+        nickname = input("CLIENT: Please enter your nickname...")
+        client.connect(('127.0.0.1', port))
+        receive_thread = threading.Thread(target=receive, args=(client, nickname,))
+        receive_thread.start()
+
+        write_thread = threading.Thread(target=write, args=(client, nickname,))
+        write_thread.start()
+    except:
+        print("ERROR: Couldn't find server... Closing client!")
+        client.close()
+
+main()
--- Server.py
+++ Server.py
@@ -1,11 +1,24 @@
 import socket
 import threading
 
-def broadcast(message, clients):
+host = '127.0.0.1'
+port = int(input("SERVER: Please input any port number between 0 to 65535."))
+while (port < 0 or port > 65535):
+    print("SERVER: Please input a valid number")
+    port = int(input())
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
     for client in clients:
         client.send(message)
 
-def handle(client, clients, nicknames):
+def handle(client):
     while True:
         try:
             message = client.recv(1024)
@@ -19,13 +32,13 @@ def handle(client, clients, nicknames):
             nicknames.remove(nickname)
             break
 
-def receive(server, clients, nicknames):
+def receive():
     while True:
         client, address = server.accept()
         print(f"SERVER: Connection with {str(address)} has been established!")
 
         client.send("NICK".encode("ascii"))
-        nickname = client.recv(1024).encode("ascii")
+        nickname = client.recv(1024).decode("ascii")
         nicknames.append(nickname)
         clients.append(client)
 
@@ -36,24 +49,6 @@ def receive(server, clients, nicknames):
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
+receive()
 
-def main():
-    """
-    DESCRIPTION: The main function that operates the TCP server
-    :return:
-    """
-    print("SERVER: Please input any port number between 0 to 65535.")
-    port = int(input())
-    while (port < 0 or port > 65535):
-        print("SERVER: Please input a valid number")
-        port = int(input())
-    print("SERVER: Initializing server... Please wait...")
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('127.0.0.1', port))
-    server.listen()
-    clients = []
-    nicknames = []
-    print("SERVER: Listening...")
-    receive(server, clients, nicknames)
 
-main()
